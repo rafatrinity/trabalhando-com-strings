782c208ef677a5aab82a27bd9c3fa6e3b9247ab2
Rafael Trindade <rafaeltrindade649@gmail.com>
2017-04-23 16:00:01 -0300

    add total de letras do nome

--- string.c
+++ string.c
@@ -6,29 +6,44 @@
 int main()
 {
 	char nome[A][B];
-	int total[A], i, j;
+	int total[A], tam, i, j;
 	for (i = 0; i < A; i++)
 	{
 		printf("\nInforme seu nome: ");
 		fgets(nome[i],B,stdin);
-		printf("%s",nome[i]);
-		getchar();
+		//getchar();
 	}
+	for (i = 0; i < A; i++)
+	{
+		tam=-1;
+		j=0;
+		while(nome[i][j] != '\0')
+		{
+			if (nome[i][j] != ' ')
+			{
+				tam++;
+				printf("%c",nome[i][j]);
+			}
+			j++;
+		}
+		printf("%d\n",tam);
+	}
+/*
 	for (i = 0; i <=A; i++){
 		total[i]=0;
 		for(j = 0; j <=i; j++){
 	    // Caso a primeira letra do nome seja vogal
-	    	if ((nome[i][1]=='a')||(nome[i][1]=='e')||(nome[i][1]=='i')||(nome[i][1]=='o')||(nome[i][1]=='u'))
-	    	{
-	    		total[i] += i * j;
-	    	}
+			if ((nome[i][1]=='a')||(nome[i][1]=='e')||(nome[i][1]=='i')||(nome[i][1]=='o')||(nome[i][1]=='u'))
+			{
+				total[i] += i * j;
+			}
 	    // Caso a primeira letra do nome seja consoante
-	    	else{
-	    		total[i] += i + j;
-	    	}
-	  	}
-	  	printf("%d\n",total[i]);
+			else{
+				total[i] += i + j;
+			}
+		}
+		printf("%d\n",total[i]);
 	}
-
+*/
 	return 0;
-}
+} 
